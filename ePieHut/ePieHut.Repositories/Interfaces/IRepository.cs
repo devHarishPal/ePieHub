e45e54ecadd0c40faab799b462b7cd6564173c85
DESKTOP-CJ2TM4F\Administrator <palharish003@gmail.com>
2024-07-01 16:28:22 +0530

    Repo added by Harish Pal

---
+++ ePieHut/ePieHut.Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ePieHut.Repositories.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        IEnumerable<TEntity> GetAll();
+
+        TEntity FetchById(int id);
+
+        void Add(TEntity entity);
+
+        void Update(TEntity entity);
+
+        void Delete(int id);
+
+        int SaveChanges();
+        
+
+
+
+
+
+
+    }
+}
---
+++ ePieHut/ePieHut.WebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ePieHut.WebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
