0b02d300b90dc887f959d84384c94ca46272a327
DESKTOP-CJ2TM4F\Administrator <dev.harishpal003@gmail.com>
2024-07-11 23:44:20 +0530

    Services Added

---
+++ ePieHut/ePieHut.Services/Implementations/Services.cs
@@ -0,0 +1,47 @@
+﻿using ePieHut.Repositories.Interfaces;
+using ePieHut.Services.Interfaces;
+
+namespace ePieHut.Services.Implementations
+{
+    public class Services<TEntity> : IServices<TEntity> where TEntity : class
+    {
+        public readonly IRepository<TEntity> repo;
+
+
+        public Services(IRepository<TEntity> _repo)
+        {
+
+            repo = _repo;
+            
+        }
+
+
+        public void Add(TEntity entity)
+        {
+           repo.Add(entity);
+            repo.SaveChanges();
+        }
+
+        public void Delete(int Id)
+        {
+            repo?.Delete(Id);
+            repo.SaveChanges();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return repo.GetAll();
+        }
+
+        public TEntity GetById(int id)
+        {
+            return repo.FetchById(id);
+        }
+
+        public void Update(TEntity entity)
+        {
+            repo.Update(entity);
+            repo.SaveChanges();
+        }
+    }
+}
---
+++ ePieHut/ePieHut.Services/Interfaces/IServices.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace ePieHut.Services.Interfaces
+{
+    public interface IServices<TEntity> where TEntity : class
+    {
+        IEnumerable<TEntity> GetAll();
+
+        TEntity GetById(int id);
+
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(int Id);
+
+
+
+
+
+
+
+    }
+}
